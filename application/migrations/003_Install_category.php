03f56b0a58159fcbae04f6ed36243235ac49dd38
Manuel Andreo Garcia <andreo.garcia@googlemail.com>
2015-10-25 00:26:07 +0200

    added stocklist

---
+++ application/migrations/003_Install_category.php
@@ -0,0 +1,44 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Migration_Install_category extends CI_Migration
+{
+	public function up()
+	{
+		$this->dbforge->add_field(
+			array(
+				'category_id' => array(
+					'type' => 'INT',
+					'constraint' => 11,
+					'unsigned' => TRUE,
+					'auto_increment' => TRUE
+				),
+				'name' => array(
+					'type' => 'VARCHAR',
+					'constraint' => 255
+				),
+				'Parent' => array(
+					'type' => 'INT',
+					'constraint' => 11,
+					'unsigned' => TRUE
+				)
+			)
+		);
+
+		$this->dbforge->add_key('category_id', true);
+		$this->dbforge->add_key('Parent');
+		$this->dbforge->create_table('category');
+
+		$this->db->query('
+			ALTER TABLE category
+				ADD CONSTRAINT fk_idx_category_Parent FOREIGN KEY (Parent) REFERENCES category(category_id) ON DELETE CASCADE;
+		');
+	}
+
+	public function down()
+	{
+		$this->dbforge->drop_table('category');
+	}
+
+}
