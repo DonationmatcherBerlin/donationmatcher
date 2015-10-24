e8afccc108a987632496166e13a2f8be052b06f4
marius.wessel <marius@liquidsquare.de>
2015-10-24 21:38:44 +0200

    named landigpage and white page for starting

---
+++ application/views/landingpage.php
@@ -0,0 +1,13 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Donationmatcher</title>
+	<link rel="stylesheet" type="text/css" href="/assets/css/bootstrap.css">
+</head>
+<body>
+
+</body>
+</html>
