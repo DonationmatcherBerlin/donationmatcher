548ba5f2ca4bf09f10773a37a87ef6e67a64e30a
Vincent van den Eijkel <van.den.eijkel@anleiter.de>
2015-10-24 22:52:10 +0200

    Create migration controller
    Set database to latest migration, open "/migrate"

--- application/controllers/Migrate.php
+++ application/controllers/Migrate.php
@@ -3,16 +3,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Migrate extends CI_Controller {
 
-	public function index()
-	{
-		$this->load->view('welcome_message');
-	}
-
-	public function up(){
+	public function index(){
 
 		$this->load->library('migration');
 
-		if ( ! $this->migration->current())
+		if ( ! $this->migration->latest())
 		{
 			show_error($this->migration->error_string());
 		}
