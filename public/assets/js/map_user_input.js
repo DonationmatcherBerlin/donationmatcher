6217a6e742a967b0e17739bac56b5e93693ec2fd
Vincent van den Eijkel <van.den.eijkel@anleiter.de>
2015-10-25 13:59:08 +0100

    Revert wrong merge

--- public/assets/js/map_user_input.js
+++ public/assets/js/map_user_input.js
@@ -1,7 +1,7 @@
 (function($) {
-	$.fn.map_user_input = function(baseInput) {
+	$.fn.mapUserName = function(baseInput) {
 		var $self = $(this);
-		var onInput = function() {
+		function onInput() {
 			$self.val($(this).val().toLowerCase()
 						.replace(/ä/g, 'ae')
 						.replace(/ö/g, 'oe')
@@ -9,7 +9,12 @@
 						.replace(/ß/g, 'ss')
 						.replace(/[^a-z0-9]/g, ''));
 		};
+		function offInput() {
+			baseInput.off('input', onInput);
+			$self.off('input', offInput);
+		};
+		baseInput = $(baseInput);
 		baseInput.on('input', onInput);
-		$self.on('input', function() { baseInput.off('input', onInput); });
+		$self.on('input', offInput);
 	};
 })(jQuery);
