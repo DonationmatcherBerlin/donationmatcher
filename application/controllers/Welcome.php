5410b912e073de687bd0d9e12fa4311b4bfd1dd9
Vincent van den Eijkel <van.den.eijkel@anleiter.de>
2015-10-25 15:19:02 +0100

    Link welcome page

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -3,31 +3,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Welcome extends CI_Controller {
 
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see http://codeigniter.com/user_guide/general/urls.html
-	 */
+
 	public function index()
 	{
-		$this->load->view('header');
-		$this->load->view('landingpage');
-		$this->load->view('footer');
+		$this->load->view('welcome');
 	}
 
-	public function local($group = 'LaGeSo'){
-		$this->load->view('header');
-		$this->load->view('local_view');
-		$this->load->view('footer');
-	}
 }
