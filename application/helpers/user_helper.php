c2c37c477694deeaed1a93e1924186698355779a
Vincent van den Eijkel <van.den.eijkel@anleiter.de>
2015-10-25 14:29:24 +0100

    Improve user helper

--- application/helpers/user_helper.php
+++ application/helpers/user_helper.php
@@ -4,16 +4,22 @@ if(! function_exists('check_role')){
 
 	function check_role($role){
 
+		if($role == 'logged_out'){
+
+			if (isset($_SESSION['logged_in']) && $_SESSION['logged_in']) redirect('/user/profile');
+			return true;
+		}
+
 		if($role == 'confirmed'){
 
-			if ($_SESSION['logged_in'] && $_SESSION['is_confirmed']) return true;
+			if (isset($_SESSION['logged_in']) && $_SESSION['logged_in'] && $_SESSION['is_confirmed']) return true;
 			redirect('/user/login');
 
 		}
 
 		if($role == 'admin'){
 
-			if($_SESSION['logged_in'] && $_SESSION['is_confirmed'] && $_SESSION['is_admin']) return true;
+			if(isset($_SESSION['logged_in']) && $_SESSION['logged_in'] && $_SESSION['is_confirmed'] && $_SESSION['is_admin']) return true;
 			redirect('/user/login');
 		}
 		
