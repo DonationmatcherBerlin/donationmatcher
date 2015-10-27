7be6764a21eab9848008cbf144c67997ab24c16d
Manuel Andreo Garcia <andreo.garcia@googlemail.com>
2015-10-27 22:52:53 +0100

    fixed variable escaping

--- provision.sh
+++ provision.sh
@@ -25,14 +25,14 @@ server {
   root /var/www/public;
   index index.php;
   location / {
-    try_files $uri /index.php$is_args$args;
+    try_files \$uri /index.php\$is_args\$args;
   }
   location ~ ^/index\.php(/|$) {
     fastcgi_pass unix:/var/run/php5-fpm.sock;
     fastcgi_split_path_info ^(.+\.php)(/.*)$;
     include fastcgi_params;
-    fastcgi_param SCRIPT_FILENAME $realpath_root$fastcgi_script_name;
-    fastcgi_param DOCUMENT_ROOT $realpath_root;
+    fastcgi_param SCRIPT_FILENAME \$realpath_root\$fastcgi_script_name;
+    fastcgi_param DOCUMENT_ROOT \$realpath_root;
     #fastcgi_param HTTPS on;
     #fastcgi_param CI_ENV production;
     internal;
