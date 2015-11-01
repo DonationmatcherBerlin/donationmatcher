7a640dca114b9b43954448bc6d14751e565318ee
Stefan Bauer <bauer.stefan@live.de>
2015-11-01 11:47:58 +0100

    Remove the bird :)

--- application/views/header.php
+++ application/views/header.php
@@ -37,7 +37,7 @@
 						<span class="icon-bar"></span>
 						<span class="icon-bar"></span>
 					</button>
-					<a class="navbar-brand" href="<?= base_url() ?>">Bedarfsplaner.org<i class="fa fa-twitter"></i></a>
+					<a class="navbar-brand" href="<?= base_url() ?>">Bedarfsplaner.org</a>
 				</div>
 				<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 					<ul class="nav navbar-nav navbar-right">
