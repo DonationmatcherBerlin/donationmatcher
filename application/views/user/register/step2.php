aa5b2c3b19cd66bfc4908c30ce8a501d26430952
Marius Wessel <github@mwessel.com>
2015-11-01 20:38:29 +0100

    register step 2 style changes

--- application/views/user/register/step2.php
+++ application/views/user/register/step2.php
@@ -3,16 +3,16 @@
 	<div class="row" style="margin-bottom: 200px;">
 		<div class="col-md-12">
 			<div class="page-header">
-        <div style="background-color: lightgreen;">
+				<div style="background-color: lightgreen;">
 				<div class="jumbotron" style="background-color: lightgreen ;">
-          <div class="container">
-            <h2>Vielen Dank für Eure Anmeldung!</h2>
-            <p>Wir überprüfen so schnell wie möglich Eure Angaben und schicken euch per E-Mail einen Link, den Ihr anklicken könnt, um die Regietrierung abzuschließen.</p>
-            <p>Warum machen wir das ?</p>
-			<p>Wir wollen sicherstellen, dass wirklich nur Hilfsgruppen bei bedarfsplaner.org sich miteinander vernetzen.</p>
-		  </div>
-        </div>
-        </div>
+					<div class="container">
+						<h2 class="text-center">Vielen Dank für Eure Anmeldung!</h2>
+						<p style="margin-bottom: 30px;">Wir überprüfen so schnell wie möglich Eure Angaben und schicken euch per E-Mail einen Link, den Ihr anklicken könnt, um die Regietrierung abzuschließen.</p>
+						<p class="text-center" style="margin-bottom: 0px;">Warum machen wir das ?</p>
+						<p>Wir wollen sicherstellen, dass wirklich nur Hilfsgruppen bei bedarfsplaner.org sich miteinander vernetzen.</p>
+			</div>
+				</div>
+				</div>
 		</div>
 	</div><!-- .row -->
 </div><!-- .container -->
