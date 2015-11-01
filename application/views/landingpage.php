aeee06ed029e5eca86df9a4159ece6b6c0b3ad1c
Marius Wessel <github@mwessel.com>
2015-11-01 15:17:34 +0100

    prefinal structure for the landingpage

--- application/views/landingpage.php
+++ application/views/landingpage.php
@@ -1,185 +1,126 @@
-  <head>
-    <style>
-      #map {
-        height: 250px;
-      }
-    </style>
-  </head>
-<body>
+<!-- TODO: Frontend, remove duplicated header! -->
+<head>
+	<style>
+		#map {
+			height: 250px;
+		}
+	</style>
+</head>
 
-<div class="container">
-	<div class="row doYouNeed">
-		<div class="col-sm-6 text-right">
-			<h2>Braucht ihr eigentlich noch <h2>
-		</div>
-		<div class="col-sm-3 text-left">
-			<div class="form-group">
-			  <label for="sel1"></label>
-			  <select class="form-control" id="sel1">
-			  	<option>...?</option>
-			    <option>Schuhe</option>
-			    <option>Katzen</option>
-			    <option>Oder</option>
-			    <option>So</option>
-			  </select>
+<div class="container" style="margin-top: 25px;">
+
+	<div class="row">
+		<div class="col-sm-6">
+
+			<div class="row doYouNeed">
+				<div class="row">
+					<div class="col-sm-12 text-center">
+						<h2>Braucht ihr noch:<h2>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-sm-6 col-sm-offset-3">
+						<div class="form-group">
+							<label for="sel1"></label>
+							<select class="form-control" id="sel1">
+								<!-- TODO:  Backend, Kategorienabfrage  -->
+								<option>Bitte Kategorie wählen</option>
+								<option>Schuhe</option>
+								<option>Katzen</option>
+								<option>Oder</option>
+								<option>So</option>
+							</select>
+						</div>
+					</div>
+				</div>
+			</div>  <!-- /doYouNeedRow -->
+
+			<div class="row">
+				<div class="col-sm-6 col-sm-offset-3">
+
+				<!-- TODO: Backend, Antwort auf do you need dynamisch einblenden -->
+					<div class="alert alert-success text-center" role="alert">
+						<p>Ja, brauchen wir noch.</p>
+					</div>
+					<div class="alert alert-warning text-center" role="alert">
+						<p>Brauchen wir nicht mehr.</p>
+					</div>
+
+				</div>
 			</div>
+		</div> <!-- /col-sm-6 -->
+
+		<div class="col-sm-6">
+			<!-- TABLE -->
+			<table class="table table-bordered table-striped responsive-utilities topTentable">
+				<thead>
+					<tr>
+						<th class="text-center">
+							<h3>Das brauchen wir am meisten:</h3>
+						</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td>Kleidung</td>
+					</tr>
+					<tr>
+						<td>Schuhe</td>
+					</tr>
+					<tr>
+						<td>Katzen</td>
+					</tr>
+					<tr>
+						<td>Katzen</td>
+					</tr>
+					<tr>
+						<td>Katzen</td>
+					</tr>
+					<tr>
+						<td>Katzen</td>
+					</tr>
+					<tr>
+						<td>Katzen</td>
+					</tr>
+					<tr>
+						<td>Katzen</td>
+					</tr>
+					<tr>
+						<td>Katzen</td>
+					</tr>
+					<tr>
+						<td>Katzen</td>
+					</tr>
+				</tbody>
+			</table>
 		</div>
 	</div>
 
-	<hr>
+	<hr style="margin: 50px 0px;">
 
-	<!-- TABLE -->
-	<h2 class="headline">Das brauchen wir am meisten.</h2>
-    <table class="table table-bordered table-striped responsive-utilities topTentable">
-      <thead>
-        <tr>
-          <th>
-            <i class="fa fa-trophy"></i> Top 10
-          </th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>Kleidung</td>
-        </tr>
-        <tr>
-          <td>Schuhe</td>
-        </tr>
-        <tr>
-          <td>Katzen</td>
-        </tr>
-        <tr>
-          <td>Katzen</td>
-        </tr>
-        <tr>
-          <td>Katzen</td>
-        </tr>
-        <tr>
-          <td>Katzen</td>
-        </tr>
-        <tr>
-          <td>Katzen</td>
-        </tr>
-        <tr>
-          <td>Katzen</td>
-        </tr>
-        <tr>
-          <td>Katzen</td>
-        </tr>
-        <tr>
-          <td>Katzen</td>
-        </tr>
-      </tbody>
-    </table>
+	<!-- TODO: Frontend, correct Map -->
+	<!-- TODO: Backend, addresses for map -->
+	<h2 class="headline text-center">Hier befinden sich alle Unterkünfte</h2>
+	<div id="map"></div>
 
+</div> <!-- container -->
 
-
-<hr>
-
-
-<h2 class="headline">Hier der gesamte Bedarfsplan von Berlin</h2>
-<div class="table-responsive">
-    <table class="table table-bordered table-striped responsive-utilities">
-      <thead>
-        <tr>
-          <th></th>
-          <th>
-            Extra small devices
-            <small>Phones (&lt;768px)</small>
-          </th>
-          <th>
-            Small devices
-            <small>Tablets (≥768px)</small>
-          </th>
-          <th>
-            Medium devices
-            <small>Desktops (≥992px)</small>
-          </th>
-          <th>
-            Large devices
-            <small>Desktops (≥1200px)</small>
-          </th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <th scope="row"><code>.visible-xs-*</code></th>
-          <td class="is-visible">Visible</td>
-          <td class="is-hidden">Hidden</td>
-          <td class="is-hidden">Hidden</td>
-          <td class="is-hidden">Hidden</td>
-        </tr>
-        <tr>
-          <th scope="row"><code>.visible-sm-*</code></th>
-          <td class="is-hidden">Hidden</td>
-          <td class="is-visible">Visible</td>
-          <td class="is-hidden">Hidden</td>
-          <td class="is-hidden">Hidden</td>
-        </tr>
-        <tr>
-          <th scope="row"><code>.visible-md-*</code></th>
-          <td class="is-hidden">Hidden</td>
-          <td class="is-hidden">Hidden</td>
-          <td class="is-visible">Visible</td>
-          <td class="is-hidden">Hidden</td>
-        </tr>
-        <tr>
-          <th scope="row"><code>.visible-lg-*</code></th>
-          <td class="is-hidden">Hidden</td>
-          <td class="is-hidden">Hidden</td>
-          <td class="is-hidden">Hidden</td>
-          <td class="is-visible">Visible</td>
-        </tr>
-      </tbody>
-      <tbody>
-        <tr>
-          <th scope="row"><code>.hidden-xs</code></th>
-          <td class="is-hidden">Hidden</td>
-          <td class="is-visible">Visible</td>
-          <td class="is-visible">Visible</td>
-          <td class="is-visible">Visible</td>
-        </tr>
-        <tr>
-          <th scope="row"><code>.hidden-sm</code></th>
-          <td class="is-visible">Visible</td>
-          <td class="is-hidden">Hidden</td>
-          <td class="is-visible">Visible</td>
-          <td class="is-visible">Visible</td>
-        </tr>
-        <tr>
-          <th scope="row"><code>.hidden-md</code></th>
-          <td class="is-visible">Visible</td>
-          <td class="is-visible">Visible</td>
-          <td class="is-hidden">Hidden</td>
-          <td class="is-visible">Visible</td>
-        </tr>
-        <tr>
-          <th scope="row"><code>.hidden-lg</code></th>
-          <td class="is-visible">Visible</td>
-          <td class="is-visible">Visible</td>
-          <td class="is-visible">Visible</td>
-          <td class="is-hidden">Hidden</td>
-        </tr>
-      </tbody>
-    </table>
-  </div>
-    <div id="map"></div>
-    <script>
+		<script>
 
 function initMap() {
 	//data from DB
 	var addresses = [
-    'Hauptstr.13 Berlin Germany',
-    'Turmstr.13 Berlin Germany',
-    'Eisenacherstr.13 Berlin 10777 Germany' //needed format zipcode important because of double street entries
+		'Hauptstr.13 Berlin Germany',
+		'Turmstr.13 Berlin Germany',
+		'Eisenacherstr.13 Berlin 10777 Germany' //needed format zipcode important because of double street entries
 	];	
 	//build new map
 	var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 11,
-    center: {lat: 52.493830, lng: 13.423598}
+		zoom: 11,
+		center: {lat: 52.493830, lng: 13.423598}
 	});
-	  for (i = 0; i < addresses.length; i++) {
-		  
+		for (i = 0; i < addresses.length; i++) {
+			
 		var geocoder = new google.maps.Geocoder();     //new geocoder (needed to get geolocation)
 		var address = addresses[i];			 
 
@@ -198,11 +139,9 @@ function initMap() {
 				else     
 				alert("Geocode was not successful for the following reason: " + status);        
 		});
-	  }
+		}
 }
 
-    </script>
+		</script>
 
-    <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBJ1d7DryuY_ypyZ-NIZvwla-XfJ9EiTmE&signed_in=true&callback=initMap"></script>
-
-</div> <!-- container -->
+		<script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBJ1d7DryuY_ypyZ-NIZvwla-XfJ9EiTmE&signed_in=true&callback=initMap"></script>
