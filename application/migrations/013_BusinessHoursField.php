1055c91a4bf8ba5266f7037ad83f40ca7c35ab0d
Vincent van den Eijkel <van.den.eijkel@anleiter.de>
2015-11-01 20:38:22 +0100

    BusinessHour migration incl. default value

---
+++ application/migrations/013_BusinessHoursField.php
@@ -0,0 +1,34 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Migration_BusinessHoursField extends CI_Migration
+{
+	public function up()
+	{
+
+		$db = $this->load->database('default',true);
+		$this->dbforge->modify_column('facility',
+			array(
+				'opening_hours' => array(
+					'type' => 'TEXT',
+					'default' => '[{"isActive":false,"timeFrom":null,"timeTill":null},
+					                {"isActive":false,"timeFrom":null,"timeTill":null},
+					                {"isActive":false,"timeFrom":null,"timeTill":null},
+					                {"isActive":false,"timeFrom":null,"timeTill":null},
+					                {"isActive":false,"timeFrom":null,"timeTill":null},
+					                {"isActive":false,"timeFrom":null,"timeTill":null},
+					                {"isActive":false,"timeFrom":null,"timeTill":null}]'
+				)
+			)
+		);
+		$db->query('update facility set opening_hours = \'[{"isActive":false,"timeFrom":null,"timeTill":null},{"isActive":false,"timeFrom":null,"timeTill":null},{"isActive":false,"timeFrom":null,"timeTill":null},{"isActive":false,"timeFrom":null,"timeTill":null},{"isActive":false,"timeFrom":null,"timeTill":null},{"isActive":false,"timeFrom":null,"timeTill":null},{"isActive":false,"timeFrom":null,"timeTill":null}]\';');
+
+	}
+
+	public function down()
+	{
+		// nobody cares...
+	}
+
+}
