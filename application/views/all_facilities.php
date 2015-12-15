aa92d7526c68d28555748668bf5db5a1534ee90a
Christopher Parotat <christopher@parotat.de>
2015-12-15 21:44:31 +0100

    Add data of all facilities to welcome/top view

---
+++ application/views/all_facilities.php
@@ -0,0 +1,41 @@
+<h2 class="headline">Liste der Hilfsgruppen in Berlin</h2>
+<div class="table-responsive">
+    <table class="table table-striped table-condensed">
+        <thead>
+            <tr>
+                <th class="text-center">Name</th>
+                <th class="text-center">Homepage</th>
+                <th class="text-center">Email</th>
+                <th class="text-center">Telefon</th>
+                <th class="text-center">Adresse</th>
+                <th class="text-center">Öffnungszeiten</th>
+                <th class="text-center">Bedarfsliste</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach($facilities as $facility): ?>
+                <tr>
+                    <td><?= $facility->organisation; ?></td>
+                    <td>
+                        <?php if($facility->homepage): ?>
+                            <a target="_blank" href="<?= $facility->homepage; ?>">Link</a>
+                        <?php endif; ?>
+                    </td>
+                    <td>
+                        <?php if($facility->email): ?>
+                            <a href="mailto:<?= $facility->email; ?>">Email</a>
+                        <?php endif; ?>
+                    </td>
+                    <td><?= $facility->phone; ?></td>
+                    <td>
+                        <?= $facility->address; ?><br />
+                        <?= $facility->zip; ?> <?= $facility->city; ?>
+                    </td>
+                    <td><?= opening_hours($facility->opening_hours); ?></td>
+                    <td><a href="<?= base_url('/stocklist/public_pdf/' . $facility->facility_id) ?>">Download</a></td>
+                </tr>
+            <?php endforeach; ?>
+        </tbody>
+    </table>
+</div>
+
